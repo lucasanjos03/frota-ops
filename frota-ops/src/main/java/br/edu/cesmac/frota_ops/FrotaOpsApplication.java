d107d073789c3f5532253c6fee9395195f5c38de
Felipe <felipesorrentino89@gmail.com>
2026-09-03 11:33:06 -0300

    chore: inicializa projeto com Spring Boot

---
+++ frota-ops/src/main/java/br/edu/cesmac/frota_ops/FrotaOpsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.cesmac.frota_ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotaOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotaOpsApplication.class, args);
+	}
+
+}
---
+++ frota-ops/src/test/java/br/edu/cesmac/frota_ops/FrotaOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.cesmac.frota_ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
